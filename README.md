23e617daaa78b8137d29f5c10073beda95e1f0a9
Ryan Franco <rfranco92@gmail.com>
2017-10-06 00:16:10 -0400

    made bamazonCustomer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bamazon
+
+this is an imgur of it running
+
+https://imgur.com/a/rZ58K
+
+to use, go on bamazonCustomer.js and make sure to hit the space bar so that the data shows up on inquirer first and foremost.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,8 +13,67 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-inquirer.prompt([
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+
+  connection.query("SELECT * FROM catalog", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+   }); 
+
+   start();
+ 
+});
+
+function start(){
+	inquirer.prompt([
+	  {
+        name: "id",
+        type: "input",
+        message: "What is the id of the item you would like to buy?"
+      },
+      {
+        name: "units",
+        type: "input",
+        message: "How many units do you want to buy of that product?"
+      }
+	]).then(function(response){
+	connection.query("SELECT * FROM catalog WHERE ?",
+	{
+		item_id : response.id
+	}, 
+	function(err, res) {
+    	if (err) throw err;
+    	console.log(res[0].stock_quantity);
+    	console.log(response.units);
+    	var stockRemoval = res[0].stock_quantity - response.units;
+		var total = response.units*res[0].price;
+    	console.log(stockRemoval);
+    	if(res[0].stock_quantity >= response.units){  
+    		connection.query(
+            "UPDATE catalog SET ? WHERE ?",
+            [
+              {
+               stock_quantity : stockRemoval 
+              },
+              {
+                item_id: response.id
+              }
+            ],
+    		function(error) {
+              if (error) throw error;
+    		console.log("You have made a purchase!");
+    		console.log("Your total amount due is $" + total);
+    		})
+    	}
+    	else{
+    	console.log("There is insufficient stock. Sorry!")
+    	}
+    	 connection.end();
+   		}); 	
+	});
 
 
-
-	])
+}
